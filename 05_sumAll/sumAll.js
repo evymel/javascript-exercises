da0f032a8ae53ad0b10971bbc9de66703bd1079a
Evyatar <evyatar.gingi@gmail.com>
2025-02-21 11:48:12 +0200

    fixed task 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,22 @@
 const sumAll = function(startInteger, endInteger) {
     let sum = 0 ;
-    for (let index = startInteger; index <= endInteger; index++) {
-        sum = sum + index
-    }
-    return sum ;
+    //this solves quite a lot of nifty things
+    if (Number.isInteger(startInteger) === false ||  Number.isInteger(endInteger) === false) {return 'ERROR'}
+    
+
+    else if (startInteger <= endInteger && startInteger > 0 && endInteger > 0) {
+        for (let index = startInteger; index <= endInteger; index++) {
+            sum = sum + index
+        }
+        return sum ;
+
+    } else if (startInteger > endInteger && startInteger > 0 && endInteger > 0) {
+        for (let index = endInteger; index <= startInteger; index++) {
+            sum = sum + index
+        }
+        return sum ;
+    } else {return 'ERROR'}
+
 
 
 };
