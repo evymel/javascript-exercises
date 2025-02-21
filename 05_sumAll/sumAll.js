a2dad677e76a127163479dbf678309574827cac9
Evyatar <evyatar.gingi@gmail.com>
2025-02-21 10:26:18 +0200

    nice

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,10 @@
-const sumAll = function() {
+const sumAll = function(startInteger, endInteger) {
+    let sum = 0 ;
+    for (let index = startInteger; index <= endInteger; index++) {
+        sum = sum + index
+    }
+    return sum ;
+
 
 };
 
