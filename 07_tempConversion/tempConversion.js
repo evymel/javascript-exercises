d269b9af1cd47b70bc930239a468a4f08f906ed4
Evyatar <evyatar.gingi@gmail.com>
2025-02-21 12:17:54 +0200

    exercise 7 , two points of interest, number() and the tofixed returns string so we used number() to change it back to integer

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahr) {
+  let tempInCel = ( tempInFahr - 32) * ( 5 / 9 );
+  return Number(tempInCel.toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCel) {
+  let tempInFahr = ( tempInCel * ( 9 / 5 )) + 32;
+  return Number(tempInFahr.toFixed(1));
 };
 
 // Do not edit below this line
